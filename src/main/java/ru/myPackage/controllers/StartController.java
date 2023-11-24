287fab34e582e04279ebfa7f3e64eaa48eef1354
Stepan6582 <prokofev02@bk.ru>
2023-11-24 16:34:54 +0300

    ...

--- src/main/java/ru/myPackage/controllers/StartController.java
+++ src/main/java/ru/myPackage/controllers/StartController.java
@@ -1,15 +1,26 @@
 package ru.myPackage.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.myPackage.DAO.ModelsDAO;
 
 @Controller
 @RequestMapping("/start")
 public class StartController {
 
+    private final ModelsDAO modelsDAO;
+
+    @Autowired
+    public StartController(ModelsDAO modelsDAO) {
+        this.modelsDAO = modelsDAO;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("countOfBuilders", modelsDAO.index().size());
         return "start/index";
     }
 }
